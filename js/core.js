a1551ce25aa3a01a877ee4a5a64ec67882d8683a
Digital Charlie <furman.jr@gmail.com>
2016-11-07 12:05:47 -0800

    minor text fixes

--- js/core.js
+++ js/core.js
@@ -1,8 +1,8 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-
     if (scroll >= 500) {
         $(".clearHeader").addClass("darkHeader");
+        console.log("SCROLLED!");
     } else {
         $(".clearHeader").removeClass("darkHeader");
     }
