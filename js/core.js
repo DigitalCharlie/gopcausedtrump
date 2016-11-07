a8fb8020acf19e147319d80cec9c962f2083e814
Digital Charlie <furman.jr@gmail.com>
2016-11-07 15:50:39 -0800

    trying modals

--- js/core.js
+++ js/core.js
@@ -1,9 +1,33 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500) {
-        $(".clearHeader").addClass("darkHeader");
-        console.log("SCROLLED!");
-    } else {
-        $(".clearHeader").removeClass("darkHeader");
-    }
-});
+/* SCROLL WORKING */
+
+	$(window).scroll(function() {    
+	    var scroll = $(window).scrollTop();
+	    if (scroll >= 500) {
+	        $(".clearHeader").addClass("darkHeader");
+	        console.log("SCROLLED!");
+	    } else {
+	        $(".clearHeader").removeClass("darkHeader");
+	    }
+	});
+
+/* MODAL */
+
+	$(function() {
+		$(".launchActionModal").on('click', function(event){
+			$('.modal').css('display', 'block');
+			setTimeout(function() {
+				$('.overlay').css('opacity', 1);
+			}, 50);
+		})
+
+		$("#the-x").click(function() {
+		    $('.modal').css('display', 'none');
+		    setTimeout(function() {
+		    $('.modal').css('opacity', 0);
+		    }, 50);
+		});
+	});
+
+/* THE X */
+
+
