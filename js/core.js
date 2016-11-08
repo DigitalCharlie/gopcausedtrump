c71bd49eecb50bd48a977ef83616c0af73f13039
Digital Charlie <furman.jr@gmail.com>
2016-11-08 12:42:45 -0800

    thank you page working

--- js/core.js
+++ js/core.js
@@ -57,12 +57,12 @@ $(function() {
     closeModal('#shareModal');
   });
 
-/* NEED WAY SO WHEN .can_thank_you_wrap IS ADDED TO #can_embed_form, SOMETHING ELSE IS DISPLAYED. */
-  $(document).on('click', 'input[type="submit"]', function(event) {
-    // For if you want to keep the form from actually being submitted
-    // event.preventDefault();
-    console.log(event);
-    console.log($(this));
-  });
+/* THIS IS HOW THE THANK YOU PAGE IS SHOWN */
+
+  $(document).on('can_embed_submitted', function() {
+          $('#actionModalThanks').css('display','block');
+          $('#actionModalPage1').css('display','none');
+          $('#can_thank_you h1').html('Thanks!');
+     });
 
 });
